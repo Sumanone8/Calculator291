a59082d2408753530fa066b69c1bf9fb0bcfb849
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-03 00:59:02 +0530

    Suman: basic calculator program

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fellow Learner!");
+            Console.WriteLine("Enter your Name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + ",");
+
+            Console.WriteLine("Enter the first number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = number1 + number2;
+            int difference = number1 - number2;
+            int multiplication = number1 * number2;
+            double division = (double)number1 / number2;
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Difference: " + difference);
+            Console.WriteLine("Multiplication: " + multiplication);
+            Console.WriteLine("Division: " + division);
+
+            Console.ReadLine();
+        }
+    }
+}
