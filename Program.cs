c6b5e8d8a4fa0227da1517b5c58c1c2ee2c01354
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-03 01:29:52 +0530

    Suman : Type two calculator program

--- Program.cs
+++ Program.cs
@@ -1,33 +1,41 @@
 ﻿using System;
 
-namespace Calculator
+namespace Calulator
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello Fellow Learner!");
             Console.WriteLine("Enter your Name");
             String name = Console.ReadLine();
-            Console.WriteLine("Welcome " + name + ",");
 
-            Console.WriteLine("Enter the first number:");
-            int number1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the second number:");
-            int number2 = Convert.ToInt32(Console.ReadLine());
-
-            int sum = number1 + number2;
-            int difference = number1 - number2;
-            int multiplication = number1 * number2;
-            double division = (double)number1 / number2;
-
-            Console.WriteLine("Sum: " + sum);
-            Console.WriteLine("Difference: " + difference);
-            Console.WriteLine("Multiplication: " + multiplication);
-            Console.WriteLine("Division: " + division);
+            Calculator.AdditionOfTwoNumbers();
+            Calculator.DifferenceOfTwoNumbers();
 
             Console.ReadLine();
+        }
+    }
+
+    public class Calculator
+    {
+        public static void AdditionOfTwoNumbers()
+        {
+            Console.WriteLine("Enter 1st number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum: " + (firstNumber + secondNumber));
+        }
+
+        public static void DifferenceOfTwoNumbers()
+        {
+            Console.WriteLine("Enter 1st number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Difference: " + Math.Abs(firstNumber - secondNumber));
         }
     }
 }
